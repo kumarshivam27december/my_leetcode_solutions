f2f09bc3f2a737d97898614c73bde9f6b0f708d8
Shivam Kumar <114799228+kumarshivam27december@users.noreply.github.com>
2024-01-11 19:28:11 +0530

    Add files via upload

---
+++ pr0258_add_digits.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    /*
+    int addDigits(int num) {
+        if(num==0){
+            return 0;
+        }else if(num%9==0){
+            return 9;
+        }else{
+            return num%9;
+        }
+    }
+    */
+
+
+
+    /*
+
+    int addDigits(int num) {
+        if ( num == 0 ) return 0;
+        return num%9 == 0 ? 9 : num%9 ;
+    }
+
+    */
+};
+
